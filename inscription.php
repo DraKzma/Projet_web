2ce90cb090080088e84a1a1a27d810cabc47d4ef
DraKzma <anthony.court72@gmail.com>
2025-04-03 15:29:46 +0200

    la plupart des pages php a ameliorer

---
+++ inscription.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+include "connex.inc.php";
+
+function affichage_connectee(){
+    echo "<h3>Vous êtes déjà connecté, qu'attendez vous?</h3>\n";
+}
+
+function afficheFormulaire($p){
+    echo "<form action='".htmlspecialchars($_SERVER['PHP_SELF'])."'method='post'>\n";
+    echo "<label>Pseudo: <input type='text' value='".$p."' name='pseudo' required='required'></label>\n";
+    echo "<br>\n";
+    echo "<label>Mot de passe: <input type='password' name='mdp' required='required'></label>\n";
+    echo "<br>\n";
+    echo "<button type='submit' name='envoi'>Submit</button>\n";
+    echo "<br\n>";
+    echo "</form>\n";
+}
+
+function affichage_connection(){
+    echo "<h3>Inscription réussie, amusez vous bien!</h3>\n";
+    echo "<a href='connexion.php'>Connexion</a>";
+}
+
+function choix_affichage(){
+    if(isset($_SESSION["pseudo"])){
+        affichage_connectee();
+    }
+    else{
+        if(isset($_POST["pseudo"])){
+            $pseudo = $_POST["pseudo"];
+            $mdp = $_POST["mdp"];
+
+            $base = "tresor.sqlite";
+            $pdo = connex($base);
+
+            $stmt = $pdo->prepare("SELECT pseudo FROM joueurs WHERE pseudo = :pseudo");
+            $stmt->bindParam(":pseudo", $pseudo);
+            $stmt->execute();
+            $bdd_pseudo = $stmt->fetchAll();
+            if(count($bdd_pseudo) != 0){
+                afficheFormulaire("Ce pseudo est déjà utilisé");
+            }
+            else{
+                $mdp = md5($mdp);
+                $stmt = $pdo->prepare("INSERT INTO joueurs VALUES (:pseudo, :mdp, 0, 1)");
+                $stmt->bindParam(":pseudo", $pseudo);
+                $stmt->bindParam(":mdp", $mdp);
+                $stmt->execute();
+                affichage_connection();
+            }
+            $pdo=null;
+        }
+        else{
+            afficheFormulaire(null);
+        }
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Inscription_TRESOR</title>
+    </head>
+    <body>
+
+        <?php choix_affichage(); ?>
+        
+    </body>
+</html>
---
+++ logout.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+$_SESSION["pseudo"] = NULL;
+$_SESSION["status"] = NULL;
+$_SESSION["progres"] = NULL;
+session_destroy();
+
+if(!isset($_GET["logout"])){
+    echo "<h3>Une erreure innatendue s'est produite, les devs devraient vraiment apprendre à coder..</h3>\n";
+}
+else{
+    if($_GET["logout"] == "accueil"){
+        header("Location:accueil.html");
+    }
+    else{
+        if($_GET["logout"] == "bravo"){
+            header("Location:bravo.html");
+        }
+    }
+}
+
+?>
